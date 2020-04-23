bff4ce4efcd513e496b71ae906b4c9d0dac2b7d5
Sai Priya <k.saipriya08@gmail.com>
2020-04-23 22:45:21 +0530

    initial commit

--- db_model.py
+++
@@ -1,7 +0,0 @@
-from guest_book import db
-
-
-class Comments(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    comment = db.Column(db.String(1000))
