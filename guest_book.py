ec698c14ef7ce44b95bc94f6d3231d68329e764a
Sai Priya <k.saipriya08@gmail.com>
2020-05-05 00:26:50 +0530

    made changes in guest_book

--- guest_book.py
+++ guest_book.py
@@ -35,12 +35,6 @@ def process():
     return redirect(url_for('index'))
 
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
-    return render_template('example.html', links=links)
-
-
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
